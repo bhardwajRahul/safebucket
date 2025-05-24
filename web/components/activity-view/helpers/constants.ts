903c76a1ac71b41da5420cc6ddcc1dddfe13b5fb
yohanleb <yohan.lebricquer@gmail.com>
2025-05-24 17:35:46 +0100

    Feature: Add bucket activity (#59)
    * Feature: Add bucket activity
    
    * Add file_id to file log
    
    * Typo

--- web/components/activity-view/helpers/constants.ts
+++ web/components/activity-view/helpers/constants.ts
@@ -1,34 +1,34 @@
 import { FileDiff, FileDown, FileMinus, FileUp, Share2 } from "lucide-react";
 
-import { ActivityMessage } from "@/components/activity-view/helpers/types";
+import { ActivityMessage } from "@/components/common/types/activity";
 
 export const messageMap = {
   BUCKET_CREATED: {
-    message: "Shared a bucket '%%BUCKET_NAME%%' with you.",
+    message: "Created a bucket '%%BUCKET_NAME%%' and shared it with you.",
     icon: Share2,
     iconColor: "text-green-500",
     iconBg: "bg-green-100",
   },
   FILE_UPLOADED: {
-    message: "Uploaded a file '%%USERNAME%%' to the bucket '%%BUCKET_NAME%%'.",
+    message: "Uploaded a file '%%FILE_NAME%%' on the bucket '%%BUCKET_NAME%%'.",
     icon: FileUp,
     iconColor: "text-purple-500",
     iconBg: "bg-purple-100",
   },
   FILE_DOWNLOADED: {
-    message: "Downloaded a file '%%NAME%%' from the bucket '%%BUCKET_NAME%%'.",
+    message: "Downloaded a file '%%FILE_NAME%%' on the bucket '%%BUCKET_NAME%%'.",
     icon: FileDown,
     iconColor: "text-indigo-500",
     iconBg: "bg-indigo-100",
   },
   FILE_UPDATED: {
-    message: "Updated a file '%%USERNAME%%' from the bucket '%%BUCKET_NAME%%'.",
+    message: "Updated a file '%%FILE_NAME%%' on the bucket '%%BUCKET_NAME%%'.",
     icon: FileDiff,
     iconColor: "text-amber-500",
     iconBg: "bg-amber-100",
   },
   FILE_DELETED: {
-    message: "Deleted a file '%%USERNAME%%' from the bucket '%%BUCKET_NAME%%'.",
+    message: "Deleted a file '%%FILE_NAME%%' on the bucket '%%BUCKET_NAME%%'.",
     icon: FileMinus,
     iconColor: "text-red-500",
     iconBg: "bg-red-100",
