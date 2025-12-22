fe06bfe6e04f2a11a8b79efc9bb3b24f7784212a
yohanleb <yohan.lebricquer@gmail.com>
2025-12-22 13:57:52 +0000

    feat: add rustfs support (#677)
    * (feat) add rustfs support
    
    * fix
    
    * fix
    
    * rename
    
    * fix
    
    * fix
    
    * fix
    
    * address review comments

--- internal/messaging/types.go
+++ internal/messaging/types.go
@@ -12,21 +12,32 @@ type BucketDeletionEvent struct {
 	EventName string `json:"event_name"`
 }
 
-type MinioEvent struct {
-	Records []struct {
-		EventName string `json:"eventName"`
-		S3        struct {
-			Bucket struct {
-				Name string `json:"name"`
-			} `json:"bucket"`
-			Object struct {
-				Key          string            `json:"key"`
-				Size         int64             `json:"size"`
-				ContentType  string            `json:"contentType"`
-				UserMetadata map[string]string `json:"userMetadata"`
-			} `json:"object"`
-		} `json:"s3"`
-	} `json:"Records"`
+type RustFSEvent struct {
+	EventName string              `json:"EventName"`
+	Key       string              `json:"Key"`
+	Records   []RustFSEventRecord `json:"Records"`
+}
+
+type RustFSEventRecord struct {
+	ObjectName string          `json:"object_name"`
+	BucketName string          `json:"bucket_name"`
+	EventName  string          `json:"event_name"`
+	Data       RustFSEventData `json:"data"`
+}
+
+type RustFSEventData struct {
+	EventName string `json:"eventName"`
+	S3        struct {
+		Bucket struct {
+			Name string `json:"name"`
+		} `json:"bucket"`
+		Object struct {
+			Key          string            `json:"key"`
+			Size         int64             `json:"size"`
+			ContentType  string            `json:"contentType"`
+			UserMetadata map[string]string `json:"userMetadata"`
+		} `json:"object"`
+	} `json:"s3"`
 }
 
 type GCPEvent struct {
